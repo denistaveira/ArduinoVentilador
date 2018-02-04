3b8a3a7ce5b5d9939dcb4c9641a913e9eb942521
Unknown <denis.taveira@hotmail.com>
2018-02-04 17:11:21 -0200

    Modificação js

--- controle.js
+++ controle.js
@@ -1,23 +1,19 @@
 var geral = document.getElementById('estado_geral').innerHTML
-if (geral == '0') {
-    document.getElementById('estado_geral').innerHTML = '1';
+if (geral === '0') {
     document.getElementById('botao_geral').innerHTML = '<a href=\'/?desligar_geral\' class=\'botao\'><button>Desliga_geral</button></a>';
 } else {
-    document.getElementById('estado_geral').innerHTML = '0';
     document.getElementById('botao_geral').innerHTML = '<a href=\'/?ligar_geral\' class=\'botao\'><button>Liga_geral</button></a>';
 }
 
 var vent = document.getElementById('estado_vent').innerHTML
-if (vent == '0') {
-    document.getElementById('estado_vent').innerHTML = '1';
+if (vent === '0') {
     document.getElementById('botao_vent').innerHTML = '<a href=\'/?desligar_vent\' class=\'botao\'><button>Desliga_vent</button></a>';
 } else {
-    document.getElementById('estado_vent').innerHTML = '0';
     document.getElementById('botao_vent').innerHTML = '<a href=\'/?ligar_vent\' class=\'botao\'><button>Liga_vent</button></a>';
 }
 
 var exaust = document.getElementById('estado_exaust').innerHTML
-if (exaust == '0') {
+if (exaust === '0') {
     document.getElementById('estado_exaust').innerHTML = '1';
     document.getElementById('botao_exaust').innerHTML = '<a href=\'/?desligar_exaust\' class=\'botao\'><button>Desliga_exaust</button></a>';
 } else {
@@ -26,7 +22,7 @@ if (exaust == '0') {
 }
 
 var min = document.getElementById('estado_min').innerHTML
-if (min == '0') {
+if (min === '0') {
     document.getElementById('estado_min').innerHTML = '1';
     document.getElementById('botao_min').innerHTML = '<a href=\'/?desligar_min\' class=\'botao\'><button>Desliga_min</button></a>';
 } else {
@@ -35,7 +31,7 @@ if (min == '0') {
 }
 
 var med = document.getElementById('estado_med').innerHTML
-if (med == '0') {
+if (med === '0') {
     document.getElementById('estado_med').innerHTML = '1';
     document.getElementById('botao_med').innerHTML = '<a href=\'/?desligar_med\' class=\'botao\'><button>Desliga_med</button></a>';
 } else {
@@ -44,7 +40,7 @@ if (med == '0') {
 }
 
 var max = document.getElementById('estado_max').innerHTML
-if (max == '0') {
+if (max === '0') {
     document.getElementById('estado_max').innerHTML = '1';
     document.getElementById('botao_max').innerHTML = '<a href=\'/?desligar_max\' class=\'botao\'><button>Desliga_max</button></a>';
 } else {
